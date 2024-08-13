055d66a64b67979f3ed711f28536ba02df9a341a
Tajazziah <bootht14@gmail.com>
2024-08-13 19:15:46 -0400

    files updated

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// You're a JS developer, and you've been asked to build a a tax calculation engine
+// v4 features:
+// - if income greater than 200$, ask them to show proof (ask more questions )
+// and show the proof in addition to their net income display
+// - for everybody else, show just the net income
+
+// v5: @todo
+// - when the income is greater than $200, do the tax calculation but add a $20 deduction from the gross.
+
+// v6: dele
+// - if the state is ME or NH make the taxRate 0.05
+
 
 const grossIncome = 1000;
 const maTaxRate = 0.0423;
@@ -21,41 +33,57 @@ if( state === "MA"){
 }
 
 if(state === "NH" ){
- //do NH stuff
- if( grossIncome >= 200 ){
+    //do NH stuff
+    if( grossIncome >= 200 ){
 
-    console.log(`Your gross income is ${grossIncome}`)
+        console.log(`Your gross income is ${grossIncome}`)
+        const taxDue = grossIncome * nhTaxRate;
+        const netIncome =  grossIncome - taxDue;
 
-} else {
-    // do some stuff
-    const taxDue = grossIncome * nhTaxRate;
-    const netIncome =  grossIncome - taxDue;
 
-    console.log(netIncome);
-}   //do NH stuff
+    } else {
+        // do some stuff
+        const taxDue = grossIncome * nhTaxRate;
+        const netIncome =  grossIncome - taxDue;
+
+        
+    }
     // @TODO: make this work
     console.log(`NH Tax Rate is ${nhTaxRate}`)
 
 }
 
 if(state === "ME"){
- //do ME stuff
- if( grossIncome >= 200 ){
+    // do ME suff;
+    if( grossIncome >= 200 ){
 
-    console.log(`Your gross income is ${grossIncome}`)
+        console.log(`Your gross income is ${grossIncome}`)
+        const taxDue = grossIncome * meTaxRate;
+        const netIncome =  grossIncome - taxDue;
 
-} else {
-    // do some stuff
-    const taxDue = grossIncome * nhTaxRate;
-    const netIncome =  grossIncome - taxDue;
 
-    console.log(netIncome);
-}   //do NH stuff
+    } else {
+        // do some stuff
+        const taxDue = grossIncome * meTaxRate;
+        const netIncome =  grossIncome - taxDue;
+
+        console.log(netIncome);
+    }
     // @TODO: make this work
-    console.log(`NH Tax Rate is ${nhTaxRate}`)
+    console.log(`ME Tax Rate is ${meTaxRate}`)
 
 }
+// if( grossIncome >= 200 ){
 
+//     console.log(`Your gross income is ${grossIncome}`)
+
+// } else {
+//     // do some stuff
+//     const taxDue = grossIncome * taxRate;
+//     const netIncome =  grossIncome - taxDue;
+
+//     console.log(netIncome);
+// }
 
 
 
